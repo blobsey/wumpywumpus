8fe5a016fe7098db89f0c6609f83d11b19be9528
Aaron Thomsen <aarontthomsen@gmail.com>
2020-02-23 23:24:11 -0800

    Start working on calculating breeze probabilities

--- Wumpus_World_Cpp_Shell/src/MyAI.cpp
+++ Wumpus_World_Cpp_Shell/src/MyAI.cpp
@@ -19,12 +19,16 @@
 
 #include "MyAI.hpp"
 
+using namespace std;
+
 MyAI::MyAI() : Agent()
 {
 	// ======================================================================
 	// YOUR CODE BEGINS
 	// ======================================================================
-	
+    direction = RIGHT; // Agent starts out facing right
+	currentLocation.first = 1; //Start out in bottom-left corner; Start indexing at 1, not 0
+	currentLocation.second = 1;
 	// ======================================================================
 	// YOUR CODE ENDS
 	// ======================================================================
@@ -42,9 +46,23 @@ Agent::Action MyAI::getAction
 	// ======================================================================
 	// YOUR CODE BEGINS
 	// ======================================================================
+	// Mark currentLocation as visited
+	visited.insert(make_pair(
+			make_pair(currentLocation.first, currentLocation.second),
+			LocationInfo(currentLocation.first, currentLocation.second)
+	));
 
-	//test
-	return CLIMB;
+	if (bump) {
+		// Change in x means we found maxX
+		if (lastLocation.first != currentLocation.first) {
+			maxX = currentLocation.first;
+		} else { // The bump was because we hit the top wall
+			maxY = currentLocation.second;
+		}
+	}
+
+
+	return TURN_LEFT;
 	// ======================================================================
 	// YOUR CODE ENDS
 	// ======================================================================
@@ -53,6 +71,43 @@ Agent::Action MyAI::getAction
 // ======================================================================
 // YOUR CODE BEGINS
 // ======================================================================
+set<pair<int, int>> MyAI::getFrontier() {
+	set<pair<int, int>> frontier;
+
+	for(auto i = visited.begin(); i != visited.end(); ++i) {
+		pair<int, int> location = i->first;
+		int x = location.first, y = location.second;
+
+		if ( x - 1 >= 1) {
+			frontier.insert(make_pair(x - 1, y));
+		}
+
+		if ( x + 1 <= maxX) {
+			frontier.insert(make_pair(x + 1, y));
+		}
+
+		if ( y - 1 >= 1) {
+			frontier.insert(make_pair(x, y - 1));
+		}
+
+		if (y + 1 <= maxY) {
+			frontier.insert(make_pair(x, y + 1));
+		}
+	}
+
+	return frontier;
+};
+
+void MyAI::markSafe(pair<int, int> loc) {
+	auto i = visited.find(loc);
+
+	i->second.wumpusProb = 0.0;
+	i->second.pitProb = 0.0;
+}
+
+void MyAI::updateBreezeProbabilities() {
+
+}
 
 
 // ======================================================================
--- Wumpus_World_Cpp_Shell/src/MyAI.hpp
+++ Wumpus_World_Cpp_Shell/src/MyAI.hpp
@@ -21,7 +21,12 @@
 #define MYAI_LOCK
 
 #include "Agent.hpp"
+#include <iostream>
+#include <utility>
+#include <map>
+#include <set>
 
+using namespace std;
 class MyAI : public Agent
 {
 public:
@@ -39,8 +44,31 @@ public:
 	// ======================================================================
 	// YOUR CODE BEGINS
 	// ======================================================================
-	
-	
+	enum Directions{RIGHT, UP, LEFT, DOWN};
+	Directions direction;
+
+
+	int maxX = 100, maxY = 100; // We set the max bounds for x and y directions to 100 until we find out the
+	                            // true bounds after bumping (this is like setting it to inf. since max is 7)
+
+	pair<int, int> lastLocation, currentLocation;
+
+	struct LocationInfo {
+	    LocationInfo(int x, int y) : x(x), y(y) {}
+	    int x, y;
+		bool stench = false, breeze = false, glitter = false;
+		double wumpusProb = 1.0, pitProb = 0.2;
+	};
+
+	map<pair<int, int> , LocationInfo> visited;
+
+
+
+	// The frontier consists of tiles that are adjacent to visited tiles that aren't in visited
+	set<pair<int, int>> getFrontier();
+
+	void markSafe(pair<int, int> loc);
+    void updateBreezeProbabilities();
 	// ======================================================================
 	// YOUR CODE ENDS
 	// ======================================================================
